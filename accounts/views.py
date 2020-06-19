1497173c468f7dc138f99a003d7622167139fff0
Samad Hatsijev <crysis90war@hotmail.be>
2020-06-19 14:35:54 +0200

    Rendering Data to Templates
    Affichage des données dans les fichiers .html

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
-
+from accounts.models import *
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html', {'title': 'Dashboard'})
+    context = {
+        'title': 'Dashboard',
+        'orders': Order.objects.all(),
+        'customers': Customer.objects.all(),
+        'total_customers': Customer.objects.all().count(),
+        'total_orders': Order.objects.all().count(),
+        'delivered': Order.objects.filter(status='Delivered').count(),
+        'pending': Order.objects.filter(status='Pending').count()
+    }
+
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html', {'title': 'Products'})
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'title': 'Products', 'products': products})
 
 
 def customer(request):
